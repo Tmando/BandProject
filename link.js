f89d2fc18acf8aefbe8cc6f0fe1fb228e3a09d5b
Thomas Mandorfer <t.mandorfer@gmail.com>
2017-08-22 10:33:19 +0200

    commit five

--- link.js
+++ link.js
@@ -14,6 +14,8 @@ for(var i=0;i<links.length;i++){
   console.log(i);
   tag += "<li><a href=\""+ links[i].link + "\">"+links[i].text + "</a></li>";
 }
+tag += "<li><a href=\"https://www.facebook.com/floyddivision\"><i class=\"fa fa-facebook-official\" style=\"font-size:24px\"></i></a></li>";
+tag += "<li><a href=\"https://www.youtube.com/user/viennafloyd\"<i class=\"fa fa-youtube\" style=\"font-size:24px\"></i></li>"
 function navBar(){
   $("#mainNavigation").append(tag);
 }
