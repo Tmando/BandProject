66bd4c25ec0b47572af78c8921de01f159ba70f1
Tmando <t.mandorfer@gmail.com>
2017-08-23 13:49:08 +0200

    updatedVersion1348

--- link.js
+++ link.js
@@ -3,7 +3,7 @@ var links = [
     {text:"Band",link:"band.html"},
     {text:"News",link:"news.html"},
     {text:"Live",link:"live.html"},
-    {text:"Media",link:"media.html"},
+    {text:"Media",link:"video.html"},
     {text:"Gästebuch",link:"Gästebuch.html"},
     {text:"Links",link:"links.html"},
     {text:"Presse",link:"presse.html"},
@@ -29,9 +29,18 @@ var tag = "";
 tag += "<li><a onclick=\"activateBurgerMenu()\"><span class=\"glyphicon glyphicon-menu-hamburger\" id=\"hamburgerIcon\"></span></a></li>"
 var hamburgerMenu = "<div style=\"display:none\" id=\"hamburgerMenu\">";
 for(var i=0;i<links.length;i++){
-  console.log(i);
-  tag += "<li class=\"navBarLinks\"><a href=\""+ links[i].link + "\">"+links[i].text + "</a></li>";
-  hamburgerMenu += "<li><a href=\""+ links[i].link + "\">"+links[i].text + "</a></li>";
+    if(links[i].text=="Media"){
+      tag += "<li class=\"navBarLinks dropdown\"><a data-toggle=\"dropdown\" class=\"dropdown-toggle\" href=\""+ links[i].link + "\">"+links[i].text;
+      tag += "<ul class=\"dropdown-menu\">";
+      tag +=  "<li><a href=\"video.html\">Video</a></li>";
+      tag +=  "<li><a href=\"photo.html\">Foto</a></li>";
+      tag +=  "<li><a href=\"songlist.html\">Songlist</a></li>";
+      tag += "</ul>";
+      tag += "</li>";
+    }else{
+    tag += "<li class=\"navBarLinks\"><a href=\""+ links[i].link + "\">"+links[i].text + "</a></li>";
+    hamburgerMenu += "<li><a href=\""+ links[i].link + "\">"+links[i].text + "</a></li>";
+    }
 }
 tag += "<li><a class=\"navBarLinks\" href=\"https://www.facebook.com/floyddivision\"><i class=\"fa fa-facebook-official\" style=\"font-size:24px\"></i></a></li>";
 tag += "<li><a class=\"navBarLinks\" href=\"https://www.youtube.com/user/viennafloyd\"<i class=\"fa fa-youtube\" style=\"font-size:24px\"></i></li>";
